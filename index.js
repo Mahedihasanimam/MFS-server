cfd36d985e5b4603165a52df616beeb4d2d80c90
mahedi066 <imam25349@gmail.com>
2024-07-17 20:26:04 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // To parse JSON request bodies
+app.use(express.json());
 
-// Sample route to test the server
+const uri = `mongodb+srv://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASS}@cluster0.k4th77t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // const userdb=client.connect
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('mfs server is running');
+  res.send('mfs server is running');
 });
 
-// Route to handle form submission
-app.post('/register', (req, res) => {
-    const { name, pin, phone, email } = req.body;
-    // You can add your logic here to handle registration data
-    console.log("Received data:", { name, pin, phone, email });
+// app.post('/register', (req, res) => {
+//   const { name, pin, phone, email } = req.body;
+//   // You can add your logic here to handle registration data
+//   console.log("Received data:", { name, pin, phone, email });
 
-    // Send a response back to the client
-    res.json({ message: 'Registration data received', data: req.body });
-});
+//   // Send a response back to the client
+//   res.json({ message: 'Registration data received', data: req.body });
+// });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
